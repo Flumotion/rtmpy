c76329048c1d3f5f2b4cd79d7a089b2888d09b45
Josep Joan Ribas <jribas@flumotion.com>
2011-03-02 09:32:56 +0100

    Stop parsing a message if we catch a UnicodeError

--- rtmpy/message.py
+++ rtmpy/message.py
@@ -596,7 +596,16 @@ class Notify(Message):
         decoder = pyamf.get_decoder(pyamf.AMF0, stream=buf)
 
         self.name = decoder.next()
-        self.argv = [x for x in decoder]
+        self.argv = []
+        while 1:
+            try:
+                x = decoder.next()
+                self.argv.append(x)
+            except StopIteration:
+                break
+            except UnicodeDecodeError:
+                break
+
 
 
     def encode(self, buf):
